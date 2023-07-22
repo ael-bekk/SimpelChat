b53309f2fce12d9a190bd1cffd9824ffa6f1e080
Abdellah El bekkali <ael-bekk@e1r3p9.1337.ma>
2023-07-22 20:02:53 +0100

    init

---
+++ client.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h> // header file for network-related functions and structures (e.g., getaddrinfo)
+#include <netinet/in.h> // header file for internet protocol-related functions and structures (e.g., sockaddr_in)
+#include <sys/socket.h> // header file for socket programming functions and structures (e.g., socket, bind, listen, accept)
+#include <arpa/inet.h> // header file for internet protocol-related functions (e.g., inet_addr, inet_ntoa)
+#include <fcntl.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <string.h>
+
+#define Color_Off "\033[0m"
+#define BBlack "\033[1;30m"
+#define BRed "\033[1;31m"
+#define BGreen "\033[1;32m"
+#define BYellow "\033[1;33m"
+#define BBlue "\033[1;34m"
+#define BPurple "\033[1;35m"
+#define BCyan "\033[1;36m"
+#define BWhite "\033[1;37m"
+
+#define PORT "3490"
+#define BUFFERSIZE 100
+
+using namespace std;
+struct sockaddr_in serv_addr;
+
+char ToSend[10000], rcv[10000];
+int inc;
+
+class _info {
+    private:
+        int tab;
+        int sock;
+        string name;
+        string msg_to;
+        string old_to_send;
+        set<string> list_users;
+    public:
+        _info()                             : tab(0), sock(0) {}
+        set<string> get_list_users()        { return this->list_users; }
+        string get_old_to_send()            { return this->old_to_send; }
+        string get_msg_to()                 { return this->msg_to; }
+        string get_name()                   { return this->name; }
+        int get_sock()                      { return this->sock; }
+        int get_tab()                       { return this->tab; }
+
+        void remove_list_users(string rmv)  { this->list_users.erase(find(list_users.begin(), list_users.end(), rmv)); }
+        void set_list_users(string add)     { this->list_users.insert(add); }
+        void set_old_to_send(string to_send){ this->old_to_send = to_send; }
+        void set_msg_to(string to)          { this->msg_to = to; }
+        void set_name(string name)          { this->name = name; }
+        void set_sock(int sock)             { this->sock = sock; }
+        void set_tab(int tab)               { this->tab = tab; }
+
+        void    hid_prev_msg(int hide) { for (int k = 0; k < hide; k++) write(1, " ", 1); }
+
+        void    msg_sending(string back_stay, string color, string to_send, short pass = 0)   {
+            if (pass == 2 && !name.empty())
+                cout << back_stay << color << "Me -> \'" << Color_Off << to_send.substr(to_send.find(' ') + 1, to_send.length() - to_send.find(' ') - 2) << color << " \' " << BRed << "==>" << Color_Off << " : " << BPurple << "@" << BWhite << to_send.substr(1, to_send.find(' ')) << Color_Off << ": " << endl;
+            else if (pass == 3)
+                cout << back_stay << color << "Me -> " << Color_Off << to_send << flush;
+            else if (to_send.find(" ") == string::npos || pass == 1) {
+                cout << back_stay << color << "Me -> " << Color_Off << to_send << flush;
+                hid_prev_msg(15);
+                cout << "\r" << color << "Me -> " << Color_Off << to_send << flush;
+            }
+            else if (!pass) {
+                cout << back_stay << color << "Me -> \'" << Color_Off << to_send.substr(to_send.find(' ') + 1) << color << " \' " << BRed << "==>" << Color_Off << " : " << BPurple << "@" << BWhite << to_send.substr(1, to_send.find(' ')) << Color_Off << ": " << flush;
+                cout << "\r" << color << "Me -> \'" << Color_Off << to_send.substr(to_send.find(' ') + 1) << flush;
+            }
+        }
+
+}   info;
+
+void *receive(void *t) {
+    while (true)
+    {
+        int i = 0;
+        memset(rcv, 0, 10000);
+
+        while(read(info.get_sock(), rcv + i, 1) > 0)
+            if (*(rcv + i) == -2 || *(rcv + i) == -4) {
+                
+                int sav = *(rcv + i); *(rcv + i) = 0;
+
+                if (sav == -2)       info.set_list_users(rcv);
+                else if (sav == -4)  info.remove_list_users(rcv);
+
+                memset(rcv, 0, 10000); i = 0;
+            } else if (*(rcv + i++) == '\n') break;
+        info.set_tab(0);
+        cout << "\r" << rcv << flush;
+        info.hid_prev_msg(int(strlen(ToSend)) - int(strlen(rcv)));
+        info.msg_sending("", BGreen, ToSend);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    struct termios old_term, new_term;
+    tcgetattr(0, &old_term);
+    new_term = old_term;
+    new_term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(0, TCSANOW, &new_term);
+
+    if (argc!=3) {
+        cerr << "usage : " << argv[0] << " <IP> <port> \n";
+        exit(1);
+    }
+    int sock;
+    if ((sock=socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        cerr << "socket() error!\n";
+        exit(1);
+    }
+    info.set_sock(sock);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        cerr << "connect() error!\n";
+        exit(1);
+    }
+    pthread_t trd;
+    pthread_create(&trd, NULL, &receive, NULL);
+
+    while (true) {
+        inc = 0;
+        memset(ToSend, 0, 10000);
+
+        cout << BGreen << "Me -> " << Color_Off << flush;
+
+        while(read(0, ToSend + inc, 1) > 0)
+            if (*(ToSend + inc++) == '\n') {
+                if ((string(ToSend).find(" ") == string::npos && !info.get_name().empty()) || inc == 1)
+                    *(ToSend + --inc) = 0;
+                else
+                    break;
+            } else if (*ToSend != '@' && !info.get_name().empty()) {
+                *(ToSend + --inc) = 0;
+            } else if (*ToSend == '@' && (inc == 1 || *(ToSend + inc - 1) == '\t')) {
+                if (*(ToSend + inc - 1) == '\t') *(ToSend + --inc) = 0;
+                string to, crnt;
+                int pos = 0, users = 0;
+                set<string> list = info.get_list_users();
+                for (auto &x : list) {
+                    if ((pos = x.find(string(ToSend).substr(1))) != string::npos)
+                        to = x, users++;
+                    if (info.get_tab() && (pos = x.find(info.get_old_to_send().substr(1))) != string::npos)
+                        if (users == info.get_tab() % list.size())
+                            crnt = x;
+                }
+                if (!info.get_tab())
+                {
+                    info.set_old_to_send(ToSend);
+                    if (users > 1) {
+                        cout << "\r" << flush;
+                        for (int x = 0; x < 30; x++) cout << "          " << flush;
+                        cout << "\rusers : " << flush;
+                        for (auto &x : list)
+                            if ((pos = x.find(string(ToSend).substr(1))) != string::npos)
+                                cout << "@" << x.substr(0, pos) << BRed << x.substr(pos, inc - 1) << Color_Off << x.substr(pos+inc - 1) << "    " << flush;
+                    }
+                    if (users > 1)
+                        cout << endl;
+                    else if (users == 1)
+                        inc = strlen(string("@" + to + " ").c_str()), memcpy(ToSend, string("@" + to + " ").c_str(), inc);
+                    info.msg_sending("\r", BGreen, ToSend);
+                } else {
+                    memset(ToSend, 0, 10000);
+                    inc = strlen(string("@" + crnt).c_str()), memcpy(ToSend, string("@" + crnt).c_str(), inc);
+                    info.msg_sending("\r", BGreen, ToSend);
+                }
+                info.set_tab(info.get_tab() + 1);
+            }
+            else if (*(ToSend + inc - 1) == 127) {
+                *(ToSend + --inc) = 0;
+                if (inc) {
+                    *(ToSend + --inc) = 0;
+                    info.msg_sending("\r", BGreen, string(ToSend) + " ", 1);
+                    info.msg_sending("\r", BGreen, ToSend);
+                }
+            } else
+                info.set_tab(0),
+                info.msg_sending("\r", BGreen, ToSend);
+        
+        if (info.get_name().empty())
+            info.msg_sending("\r", BBlue, ToSend, 3),
+            info.set_name(ToSend);
+        else
+            info.msg_sending("\r", BBlue, ToSend, 2);
+        info.set_tab(0);
+        write(sock, ToSend, inc);
+    }
+    close(sock);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,274 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h> // header file for network-related functions and structures (e.g., getaddrinfo)
+#include <netinet/in.h> // header file for internet protocol-related functions and structures (e.g., sockaddr_in)
+#include <sys/socket.h> // header file for socket programming functions and structures (e.g., socket, bind, listen, accept)
+#include <arpa/inet.h> // header file for internet protocol-related functions (e.g., inet_addr, inet_ntoa)
+#include <fcntl.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <map>
+
+#define Color_Off "\033[0m"
+#define BBlack "\033[1;30m"
+#define BRed "\033[1;31m"
+#define BGreen "\033[1;32m"
+#define BYellow "\033[1;33m"
+#define BBlue "\033[1;34m"
+#define BPurple "\033[1;35m"
+#define BCyan "\033[1;36m"
+#define BWhite "\033[1;37m"
+
+#define PORT "3490"
+#define BUFFERSIZE 100
+
+using namespace std;
+
+int serv_sock;
+int clnt_sock;
+char buf;
+struct sockaddr_in serv_addr;
+struct sockaddr_in clnt_addr;
+socklen_t clnt_addr_size;
+int k = 1;
+
+int read_len=0;
+
+string snd;
+
+map<string, int> users;
+map<int, string> fd_list;
+vector<string> list_users;
+
+class client {
+    private:
+        bool    enter_name;
+        string  name;
+        char    msg[10000];
+        int     cursor;
+    public:
+        client(){
+            enter_name = 1;
+            cursor = 0;
+            memset(msg, 0, 10000);
+        }
+
+        void    get_in(int from_fd, fd_set fds, int fd_max) {
+            name = string(msg).substr(0, cursor);
+            cout << "\r" << name << " is connected" << endl;
+            cout << BGreen << "Me -> " << Color_Off << flush;
+            char c = -2;
+            for (int j = 0; j < list_users.size(); j++)
+                write(from_fd, list_users[j].c_str(), strlen(list_users[j].c_str())),
+                write(from_fd, &c, 1);
+
+            for (int j = 3; j <= fd_max; j++)
+                if (j != from_fd && FD_ISSET(j, &fds)) {
+                    write(j, name.c_str(), strlen(name.c_str()));
+                    write(j, &c, 1);
+                }
+
+            users[name] = from_fd;
+            fd_list[from_fd] = name;
+            list_users.push_back(name);
+            enter_name = 0; cursor = 0; memset(msg, 0, 10000);
+        }
+
+        void    get_out(int from_fd, fd_set fds, int fd_max) {
+            char c = -4;
+            for (int j = 1; j <= fd_max; j++)
+                if (j != from_fd && FD_ISSET(j, &fds)) {
+                    write(j, name.c_str(), cursor);
+                    write(j, &c, 1);
+                }
+            list_users.erase(find(list_users.begin(), list_users.end(), name));
+            FD_CLR(from_fd, &fds);
+            fd_list[from_fd] = "";
+            close(from_fd);
+            cout << "\r" << name << " is disconnected" << endl;
+            enter_name = 1; cursor = 0; memset(msg, 0, 10000);
+        }
+
+        void    error(string err) {
+            cerr << err << " error!\n";
+            exit(1);
+        }
+
+        void    receive(int from_fd, fd_set fds, int fd_max) {
+            int         rd;
+            const char  *ToSend = snd.c_str();
+
+            if ((rd = read(from_fd, msg + cursor, 1)) == -1)
+                error("read()");
+            else if (!rd)
+                get_out(from_fd, fds, fd_max);
+            else if (enter_name && *(msg + cursor) == '\n')
+                get_in(from_fd, fds, fd_max);
+            else if (*(msg + cursor++) == '\n') {
+                string to = string(msg).substr(1, string(msg).find(' ') - 1);
+                if (to == "everyone") {
+                    for (int j = 3; j <= fd_max; j++)
+                        if (FD_ISSET(j, &fds) && j != from_fd)
+                            write(j, BWhite, strlen(BWhite)),
+                            write(j, fd_list[from_fd].c_str(), strlen(fd_list[from_fd].c_str())),
+                            write(j, BRed, strlen(BRed)),
+                            write(j, " --> ", 5),
+                            write(j, Color_Off, strlen(Color_Off)),
+                            write(j, msg + 10, cursor - 10);
+                    cursor = 0; memset(msg, 0, 10000); to = snd = "";
+                } else if (users[to]) {
+                    int go_to = users[to];
+                    char c = -2;
+                    for (int j = 0; j < list_users.size(); j++)
+                        if (name != list_users[j])
+                            write(from_fd, list_users[j].c_str(), strlen(list_users[j].c_str())),
+                            write(from_fd, &c, 1);
+                    cout << "\r" << BWhite << name << " -> " << Color_Off << flush;
+                    write(1, msg, cursor - 1);
+                    for (int k = -2; k < int(strlen(ToSend)) - int(strlen(msg)); k++)
+                        write(1, " ", 1);
+                    write(1, "\n", 1);
+                    cout << BGreen << "Me -> " << Color_Off << flush;
+                    cout << ToSend << flush;
+                    if (FD_ISSET(go_to, &fds))
+                        write(go_to, BWhite, strlen(BWhite)),
+                        write(go_to, fd_list[from_fd].c_str(), strlen(fd_list[from_fd].c_str())),
+                        write(go_to, BRed, strlen(BRed)),
+                        write(go_to, " --> ", 5),
+                        write(go_to, Color_Off, strlen(Color_Off)),
+                        write(go_to, msg + strlen(to.c_str()) + 2, cursor - strlen(to.c_str()) - 2);
+                    cursor = 0; memset(msg, 0, 10000);
+                }
+            }
+        }
+
+        void    send(int from_fd, int to_fd, fd_set fds, int fd_max) {
+
+            if (read(0, msg + cursor, 1) == -1)
+                error("read()");
+
+            snd += *(msg + cursor);
+            if (*(msg + cursor++) == '\n') {
+                cout << "\r" << BBlue << "Me -> " << Color_Off << msg;
+
+                for (int j = 1; j <= fd_max; j++)
+                    if (FD_ISSET(j, &fds))
+                        write(j, msg, cursor);
+
+                cout << BGreen << "Me -> " << Color_Off << flush;
+
+                cursor = 0; memset(msg, 0, 10000); snd = "";
+            } else
+                write(1, (msg + cursor - 1), 1);
+        }
+
+        void set_name(string N) {
+            this->name = N;
+        }
+
+        string get_name() {
+            return this->name;
+        }
+        const char *get_msg() {
+            return string(this->msg).c_str();
+        }
+};
+
+int main(int argc, char *argv[])
+{
+    list_users.push_back("everyone");
+    fd_set fds, copy_fds;
+    int result, str_len;
+    struct timeval timeout;
+    int fd_max, fd_num;
+
+    struct termios old_term, new_term;
+    tcgetattr(0, &old_term);
+    new_term = old_term;
+    new_term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(0, TCSANOW, &new_term);
+
+    vector <client> CLNT(1024);
+    if (argc!=2) {
+        cerr << "usage : " << argv[0] << " <port> \n";
+        exit(1);
+    }
+
+    if ((serv_sock=socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        cerr << "socket() error!\n";
+        exit(1);
+    }
+
+    if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &k, sizeof(int)) < 0) {
+        cerr << "setsockopt() error!\n";
+        exit(1);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        cerr << "bind() error!\n";
+        exit(1);
+    }
+
+    if (listen(serv_sock, 5) == -1) {
+        cerr << "listen() error!\n";
+        exit(1);
+    }
+    
+    FD_ZERO(&fds);
+    FD_SET(0, &fds);
+    FD_SET(serv_sock, &fds);
+    fd_max = serv_sock;
+    
+    cout << BGreen << "Me -> " << Color_Off << flush;
+
+    while (true) {
+        copy_fds = fds;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 5000;
+
+        if ((fd_num = select(fd_max + 1, &copy_fds, 0, 0, &timeout)) == -1) {
+            cerr << "select() error!\n";
+            exit(1);
+        } else if (!fd_num)
+            continue;
+
+        for (int i = 0; i <= fd_max; i++) {
+            
+            if (FD_ISSET(i, &copy_fds)) {
+                if (i == serv_sock) {
+                    clnt_addr_size = sizeof(clnt_addr);
+                    if ((clnt_sock = accept(serv_sock, (struct sockaddr*)&serv_addr, &clnt_addr_size)) == -1) {
+                        cerr << "accept() error!\n";
+                        exit(1);
+                    }
+                    FD_SET(clnt_sock, &fds);
+                    fd_max = max(clnt_sock, fd_max);
+                    write(clnt_sock, "\rplease enter your name :\n", 27);
+                } else if (!i)
+                    CLNT[i].send(0, 0, fds, fd_max);
+                else
+                    CLNT[i].receive(i, fds, fd_max);
+            } 
+        }
+    }
+    close(clnt_sock);
+    close(serv_sock);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
+    return 0;
+}
